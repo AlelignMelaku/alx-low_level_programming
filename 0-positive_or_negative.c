e4c4f6d5e8391775b280dd078964db0beb248407
AleignMelaku <melaku813@gmail.com>
2023-06-16 00:38:52 -0700

    0x01-variables_if_else_while

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-/** 
+
+/**
  * main - deterimine whether n is positve ,negative or 0
  *
  * return (0);
@@ -7,10 +8,13 @@
 int main(void)
 {
 	int n;
-	if(n>0){
-	       	printf("is positive\n");
+
+	if (n > 0)
+	{
+		printf("is positive\n");
 	}
-	else if(n ==0){
+	else if (n == 0)
+	{
 		prinf("is zero\n");
 	}
 	else
