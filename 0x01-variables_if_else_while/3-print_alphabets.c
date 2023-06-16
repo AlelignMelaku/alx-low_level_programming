5ed5cd50959cbf0b5470235f3c9d9537e9df4a86
AleignMelaku <melaku813@gmail.com>
2023-06-16 07:57:02 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+ * main - printing Letters Alphabetically both like in abcd..AB
+ *
+ * Return: Always (0) sucess.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
