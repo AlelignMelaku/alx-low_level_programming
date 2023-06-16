4cc13e0e8cb1985df385b6d17049a3358ede2fbf
AleignMelaku <melaku813@gmail.com>
2023-06-16 13:41:34 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,14 +23,10 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 				n, n % 10);
 	}
-	else if((n %10 ) == 0))
+	else
 	{
 		printf("Last digit of %d is %d and is 0\n",
 				n, n % 10);
-	}
-	else 
-	{
-		printf("Last digit of ");
 	}
 	return (0);
 }
