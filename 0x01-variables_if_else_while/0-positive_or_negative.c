5294a8b3870653325a1bba6276606f9ba4feda1a
AleignMelaku <melaku813@gmail.com>
2023-06-16 00:51:38 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - f esle statment
+ * retun : 0
+ */
+int main(void)
+{
+	int n;
+	
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		prinf("is zero\n");
+	}
+	else
+	{
+		prinf("is negative\n");
+	}
+}
