c88eeea6a0c0bb465f049ef7bd7d89cdff46c1a8
AleignMelaku <melaku813@gmail.com>
2023-06-16 09:14:23 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main -entery points.
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int  letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
