cfdf4312d64605d71e9ab77395d2c9de614cd731
AleignMelaku <melaku813@gmail.com>
2023-06-16 12:46:25 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main -prints all possible combinations of two different digit numbers.
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1  = 0; num1 <= 8; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 8 && num2 == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
