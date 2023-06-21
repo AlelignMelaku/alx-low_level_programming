b396adddcc697de1f99aa7c87de978ac92cb67dc
AleignMelaku <melaku813@gmail.com>
2023-06-21 09:07:26 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
 	else if (b > c)
 	{
-		largest = b
+		largest = b;
 	}
 	else
 	{
 		largest = c;
 	}
-	return (largest);
+return (largest);
 }
