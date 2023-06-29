cf330cfaf844aa3ecb079fa12117f46ef78109f1
AleignMelaku <melaku813@gmail.com>
2023-06-29 09:28:47 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_buffer - add 2 strings.
+ * @b: str1.
+ * @size: str2.
+ * Return: String with all letters in ROT13 base.
+ */
+void print_buffer(char *b, int size)
+{
+
+}
