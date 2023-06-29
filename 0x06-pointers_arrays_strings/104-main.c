d5d016836b98fb67ccacc7927baf2bf05620e3e7
AleignMelaku <melaku813@gmail.com>
2023-06-29 04:32:29 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char buffer[] = "This is a string!\0And this is the\n";
+
+	printf("%s\n", buffer);
+	printf("-------------------\n");
+	print_buffer(buffer, sizeof(buffer));
+	return (0);
+}
