74f18cf6d3745efd2dd060e739de3100a96e009b
AlelignMelaku <melaku813@gmail.com>
2023-08-01 07:27:33 -0700

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * find_listint_loop - Finds the loop 
+ * @head: A pointer to the head 
+ * Return: NULL  or node
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *m, *b;
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	m = head->next;
+	b = (head->next)->next;
+	while (b)
+	{
+		if (m == b)
+		{
+			m = head;
+			while (m != b)
+			{
+				m = m->next;
+				b = b->next;
+			}
+			return (m);
+		}
+		m = m->next;
+		b = (b->next)->next;
+	}
+	return (NULL);
+}
