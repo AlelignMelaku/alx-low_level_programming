a6f1f1ac66a9811525f9dcedb5b442db2007813f
AlelignMelaku <melaku813@gmail.com>
2023-08-01 03:56:39 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+/* Auth:Melaku Alelign: 2023*/
+#include "lists.h"
+/**
+ * get_nodeint_at_index - Locates a given node from the list
+ * @head: A pointer the addres head
+ * @index: The index of the node to locate - indices start at 0.
+ * Return: NULL or node.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int get;
+
+	for (get = 0; get < index; get++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
